a138f469f6a1acf2a7603ce3cb4b97735cd414df
AmbujaAK <itsambuja@gmail.com>
2020-09-17 23:13:28 +0530

    transaction add btn created

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expenses_flutter/models/transaction.dart';
-import 'package:intl/intl.dart';
+import 'package:personal_expenses_flutter/widgets/user_transaction.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'new shoes',
-      amount: 65.0,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'new shirt',
-      amount: 55.0,
-      date: DateTime.now(),
-    ),
-  ];
-
-  // String titleInput;
-  // String amountinput;
-
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
         body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text(
-                    'Chart',
-                  ),
-                  elevation: 5,
-                ),
-              ),
-              Card(
-                elevation: 5,
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Title'),
-                        controller: titleController,
-                        // onChanged: (value)=>titleInput = value,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Amount'),
-                        controller: amountController,
-                        // onChanged: (val) => amountinput = val,
-                      ),
-                      FlatButton(
-                        onPressed: () {
-                          print(titleController.text);
-                        },
-                        child: Text('Add Transaction'),
-                        color: Colors.purple,
-                      ),
-                    ],
+          child: SingleChildScrollView(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  width: double.infinity,
+                  child: Card(
+                    color: Colors.blue,
+                    child: Text(
+                      'Chart',
+                    ),
+                    elevation: 5,
                   ),
                 ),
-              ),
-              Container(
-                child: Column(
-                  children: transactions.map((tx) {
-                    return Card(
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                              vertical: 10.0,
-                              horizontal: 15.0,
-                            ),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.purple,
-                                width: 2,
-                              ),
-                            ),
-                            padding: EdgeInsets.all(10.0),
-                            child: Text(
-                              '\$${tx.amount}',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20.0,
-                                  color: Colors.purple),
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                tx.title,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                DateFormat().format(tx.date),
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                ),
-              )
-            ],
+                UserTransaction()
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  final Function addTx;
+  NewTransaction(this.addTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+              // onChanged: (value)=>titleInput = value,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+              // onChanged: (val) => amountinput = val,
+            ),
+            FlatButton(
+              onPressed: () {
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
+              child: Text('Add Transaction'),
+              color: Colors.purple,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
