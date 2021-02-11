177c2f9749f2f0f34933ad6c6407af2895183cdb
AmbujaAK <itsambuja@gmail.com>
2021-02-11 17:08:42 +0530

    update issue fixes

--- lib/widgets/adaptive_flat_button.dart
+++ lib/widgets/adaptive_flat_button.dart
@@ -21,15 +21,23 @@ class AdaptiveFlatButton extends StatelessWidget {
             ),
             onPressed: handler,
           )
-        : FlatButton(
-            textColor: Theme.of(context).primaryColor,
+        : TextButton(
+            onPressed: handler,
             child: Text(
               text,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            onPressed: handler,
           );
   }
 }
+
+// FlatButton(
+//   textColor: Theme.of(context).primaryColor,
+//   child: Text(
+//     text,
+//     style: TextStyle(
+//       fontWeight: FontWeight.bold,
+//     ),
+//   ),
+//   onPressed: handler,
+// );
